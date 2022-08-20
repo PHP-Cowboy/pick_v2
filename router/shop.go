9b7378f46d6f9bdf2b750440ef9e8a81de0a4617
chenjie <chenjie@xsq.com>
2022-08-20 12:41:51 +0800

    门店列表权限

--- router/shop.go
+++ router/shop.go
@@ -12,14 +12,14 @@ func ShopRoute(g *gin.RouterGroup) {
 	{
 		//线路名称列表
 		group.GET("line_list", handler.LineList)
+		//门店列表
+		group.GET("/list", handler.ShopList)
 	}
 
 	shopGroup := g.Group("/shop", middlewares.JWTAuth(), middlewares.IsSuperAdminAuth())
 	{
 		//同步门店
 		shopGroup.GET("/sync", handler.SyncShop)
-		//门店列表
-		shopGroup.GET("/list", handler.ShopList)
 		//批量设置线路
 		shopGroup.POST("batch_set", handler.BatchSetLine)
 
