1ba037b48a5d44d730e53049e053a64b870fefd6
chenjie <chenjie@xsq.com>
2022-09-18 15:05:30 +0800

    mq

--- mq/main.go
+++ mq/main.go
@@ -12,7 +12,7 @@ import (
 
 func main() {
 	p, _ := rocketmq.NewProducer(
-		producer.WithNsResolver(primitive.NewPassthroughResolver([]string{"47.114.60.210:10007"})),
+		producer.WithNsResolver(primitive.NewPassthroughResolver([]string{"127.0.0.1:10007"})),
 		producer.WithRetry(2),
 	)
 
@@ -25,24 +25,12 @@ func main() {
 
 	topic := "purchase_order"
 
-	//for i := 276; i < 291; i++ {
-	//
-	//	msg := &primitive.Message{
-	//		Topic: topic,
-	//		Body:  []byte(strconv.Itoa(i)),
-	//	}
-	//
-	//	res, err := p.SendSync(context.Background(), msg)
-	//
-	//	if err != nil {
-	//		fmt.Printf("send message error: %s\n", err)
-	//	} else {
-	//		fmt.Printf("send message success: result=%s\n", res.String())
-	//	}
-	//}
-
-	toBeShipped := []int{27932, 28045, 28091, 28134, 26969, 27375, 28034, 7749, 18223, 27936, 28158, 14548, 15353, 27829, 27673, 28016, 28096, 27642, 28127, 28059, 28060, 27216, 27942, 28180, 27892, 28005, 28135, 26407, 28109}
-
+	toBeShipped := []int{
+		27595, 28352, 28176, 28283, 28239, 28268, 28139, 28150, 28270, 28292,
+		28192, 14902, 23055, 28351, 28353, 28354, 28362, 28123, 28125, 28128,
+		27647, 28242, 28360, 27579, 28306, 28146, 28279, 25493, 28300, 28314,
+		18418, 23261, 22123, 28074, 26584, 27631, 28309,
+	}
 	for _, v := range toBeShipped {
 		msg := &primitive.Message{
 			Topic: topic,
